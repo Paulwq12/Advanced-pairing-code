cb77f82ca5e041dcb63a242b3ea82f0b5d180b07
Paulwq12 <gracetacha1@gmail.com>
2025-01-10 11:58:02 +0100

    Update qrcode.js

--- js/qrcode.js
+++ js/qrcode.js
@@ -1,13 +1,11 @@
-
-        var timeleft = 30;
-        var downloadTimer = setInterval(function () {
-            if (timeleft <= 0) {
-                clearInterval(downloadTimer);
-                document.getElementById("progressBar").remove();
-                document.getElementById("main").innerHTML = "QR Expired! Please reload";
-                document.getElementById("legend").innerHTML = ""
-            }
-            document.getElementById("progressBar").value = 45 - timeleft;
-            timeleft -= 1;
-        }, 1000);
-    
+var timeleft = 60; // Extend the time to 60 seconds
+var downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        document.getElementById("progressBar").remove();
+        document.getElementById("main").innerHTML = "QR Expired! Please reload";
+        document.getElementById("legend").innerHTML = "";
+    }
+    document.getElementById("progressBar").value = 60 - timeleft; // Update progress bar calculation
+    timeleft -= 1;
+}, 1000);
